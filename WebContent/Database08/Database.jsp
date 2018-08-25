d8a30f349dab3e4a44165ed517d775bf507e8442
jinseong <pjs0101@caelumhouse.com>
2018-08-25 14:35:50 +0900

    업데이트
    버그 수정 못함

--- WebContent/Database08/Database.jsp
+++ WebContent/Database08/Database.jsp
@@ -11,8 +11,8 @@
    	<fieldset>
 		<legend>데이타베이스 연동<span style="font-size:2em;color:red">${message}${param.method}</span></legend>
 		<ul style="list-style-type: decimal;">			
-			<li><a href="<c:url value='/Database/JDBCConnection.do?method=JDBC'/>">스프링 JDBC API사용(스프링 DAO모듈에 포함됨.)</a></li>
-			<li><a href="<c:url value='/Database/JNDIConnection.do?method=JNDI'/>">스프링 JNDI API사용(스프링 CONTEXT모듈에 포함됨.)</a></li>
+			<li><a href="<c:url value='/Control/JDBCConnection.do?method=JDBC'/>">스프링 JDBC API사용(스프링 DAO모듈에 포함됨.)</a></li>
+			<li><a href="<c:url value='/Control/JNDIConnection.do?method=JNDI'/>">스프링 JNDI API사용(스프링 CONTEXT모듈에 포함됨.)</a></li>
 		</ul>
 	</fieldset>
 </body>
--- src/controller/MemberSystem.java
+++ src/controller/MemberSystem.java
@@ -1,5 +1,6 @@
 package controller;
 
+import java.sql.Connection;
 import java.util.Map;
 
 import javax.annotation.Resource;
@@ -8,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.sql.DataSource;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -67,5 +69,38 @@ public class MemberSystem{
 		return "/InSite/NoticeBoard.jsp";
 	}
 	
+	
+	//데이타 소스를 자동으로 주입 받기 위한(와이어링) 멤버변수(속성) 선언]
+	@Resource(name="dataSourceByJDBC")
+	private DataSource dataSourceJDBC;
+	
+	@Resource(name="dataSourceByJNDI")
+	private DataSource dataSourceJNDI;
+	
+	
+	
+	@RequestMapping("/Control/JDBCConnection.do")
+	public String jdbc(@RequestParam String method,Model model) throws Exception{
+		//주입받은 DataSource객체로 Connection객체 얻기]
+		Connection conn=dataSourceJDBC.getConnection();
+		//데이타 저장]
+		model.addAttribute("message",conn==null ? "[데이타베이스 연결 실패]" : "[데이타베이스 연결 성공]" );
+		//커넥션 객체 메모리 해제
+		if(conn !=null) conn.close();
+		//뷰정보 반환]
+		return "/Database08/Database.jsp";
+	}/////////////
+	
+	@RequestMapping("/Control/JNDIConnection.do")
+	public String jndi(@RequestParam String method,Model model) throws Exception{
+		//주입받은 DataSource객체로 Connection객체 얻기]
+		Connection conn=dataSourceJNDI.getConnection();
+		//데이타 저장]
+		model.addAttribute("message",conn==null ? "[데이타베이스 연결 실패]" : "[데이타베이스 연결 성공]" );
+		//커넥션  풀에 커넥션객체 반납
+		if(conn !=null) conn.close();
+		//뷰정보 반환]
+		return "/Database08/Database.jsp";
+	}/////////////
 	
 }
